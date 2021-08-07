3a01978331b8d23f5ab0044c25d23f1b4dba3595
gogo-wang <rileywang0819@gmail.com>
2021-08-08 00:26:29 +0900

    update

--- backend/models.py
+++ backend/models.py
@@ -1,78 +1,83 @@
 import os
-from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer
+from flask_migrate import Migrate
 import json
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+db_name = "trivia"
+db_path = "postgresql://{}:{}@{}/{}".format(
+    'postgres', 
+    'pupu0819', 
+    'localhost:5432', 
+    db_name
+)
 
 db = SQLAlchemy()
+migrate = Migrate()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
-def setup_db(app, database_path=database_path):
+# ----------------------------------------------------------------------------#
+# setup_db: binds a flask app and a SQLAlchemy service
+# ----------------------------------------------------------------------------#
+
+def setup_db(app, database_path=db_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    migrate.init_app(app, db)
 
-'''
-Question
+# ----------------------------------------------------------------------------#
+# Question model
+# ----------------------------------------------------------------------------#
 
-'''
-class Question(db.Model):  
-  __tablename__ = 'questions'
+class Question(db.Model):
+    __tablename__ = 'questions'
+    id = Column(Integer, primary_key=True)
+    question = Column(String, nullable=False)
+    answer = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+    difficulty = Column(Integer, nullable=False)
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
 
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    def update(self):
+        db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'question': self.question,
-      'answer': self.answer,
-      'category': self.category,
-      'difficulty': self.difficulty
-    }
+    def format(self):
+        return {
+            'id': self.id,
+            'question': self.question,
+            'answer': self.answer,
+            'category': self.category,
+            'difficulty': self.difficulty
+        }
 
-'''
-Category
+# ----------------------------------------------------------------------------#
+# Category model
+# ----------------------------------------------------------------------------#
 
-'''
-class Category(db.Model):  
-  __tablename__ = 'categories'
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    type = Column(String, nullable=False)
 
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
+    def __init__(self, type):
+        self.type = type
 
-  def __init__(self, type):
-    self.type = type
-
-  def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+    def format(self):
+        return {
+            'id': self.id,
+            'type': self.type
+        }
